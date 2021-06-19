3b18722953d0d9e7159cc70dbe4b68284b882828
arek <arekrucki@hotmail.co.uk>
2021-06-19 13:43:22 +0100

    weekend coding up to add_cash

---
+++ src/pet_shop.py
@@ -0,0 +1,21 @@
+import pdb
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+
+def get_total_cash(cash):
+    return cash["admin"]["total_cash"]
+    
+
+
+def add_or_remove_cash(given_dict, cash):
+    given_dict["admin"]["total_cash"] += cash  
+
+
+
+
+    
+
+ 
