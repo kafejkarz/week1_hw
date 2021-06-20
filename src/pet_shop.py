412e460f9af74d740feea4d0dc27a5ab65bb5ca2
arek <arekrucki@hotmail.co.uk>
2021-06-20 15:44:56 +0100

    weekend hw find pet by name Arthur

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -28,11 +28,21 @@ def get_stock_count(num_of_stock):
      return len(num_of_stock["pets"])
 
     
-
- 
 def get_pets_by_breed(get_breed, breed):
     breed_count = []
     for pet in get_breed["pets"]:
         if pet["breed"] == breed:
              breed_count.append(breed)
     return breed_count
+
+def get_pets_by_breed(breed_not_fund, breed):
+    breed_count = []
+    for pet in breed_not_fund["pets"]:
+        if pet["breed"] == breed:
+             breed_count.append(breed)
+    return breed_count
+
+def find_pet_by_name(find_name, b):
+    for name in find_name["pets"]:
+        if name["name"] == "Arthur":
+            return name
